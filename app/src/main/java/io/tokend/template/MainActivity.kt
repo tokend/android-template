51eb84dcde32a2a3c430db298f178caf4a6f3631
Kostiantyn Nechvolod <kostiantyn.nechvolod@gmail.com>
2020-12-21 13:24:18 +0200

    Added default dimensions

--- app/src/main/java/io/tokend/template/MainActivity.kt
+++ app/src/main/java/io/tokend/template/MainActivity.kt
@@ -2,16 +2,10 @@ package io.tokend.template
 
 import android.os.Bundle
 import io.tokend.template.base.activity.BaseActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreateAllowed(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_main)
-
-        test_button.setOnClickListener {
-            localeManager.setLocale(Locale("en"))
-        }
     }
 }
