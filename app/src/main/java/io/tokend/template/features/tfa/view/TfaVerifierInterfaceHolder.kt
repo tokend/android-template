a785dd250ea4e60dc095b3c6bf778879f2d9fe43
Oleg Koretsky <test.radiokot@gmail.com>
2021-02-12 13:32:42 +0200

    Remove useless TFA interface

--- app/src/main/java/io/tokend/template/features/tfa/view/TfaVerifierInterfaceHolder.kt
+++
@@ -1,7 +0,0 @@
-package io.tokend.template.features.tfa.view
-
-import org.tokend.sdk.tfa.TfaVerifier
-
-interface TfaVerifierInterfaceHolder {
-    val tfaVerifierInterface: TfaVerifier.Interface?
-}
